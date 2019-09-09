541fa83b28a53a5d32cfffec8468ffe2139f534a
Ujwal Karippali Chandran <43445980+ujwalkpl@users.noreply.github.com>
2019-09-09 10:53:05 +0530

    Create Q2a.py

---
+++ partB/Q2a.py
@@ -0,0 +1,15 @@
+def AtomicDictionary():
+	dicti={"sodium":"Na","Hydrogen":"H","Helium":"He","Nitrogen":"N","Lithium":"Li","Boron":"B"}
+	print(dicti)
+	key=input("Enter a new element")
+	value=input("Enter its symbol")
+	dicti[key]=value
+	print(dicti)
+	key=input("Enter a new element")
+	value=input("Enter its symbol")
+	dicti[key]=value
+	print(dicti)
+	print("Number of elements in the dictionary is "+str(len(dicti.keys())))
+	key=input("Enter an Element")
+	print("The atomic symbol is"+str(dicti[key]))
+
