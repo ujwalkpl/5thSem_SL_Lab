6f839aa7c6abbfd9b72d2be0419e9d54621f5c07
ujwalkpl <ujwlkpl@gmail.com>
2019-09-16 10:11:21 +0530

    added 11.py

--- 11.py
+++ 11.py
@@ -2,11 +2,12 @@ strr = ["Msrit","sounds","cooler","than","Rit"]
 li= [i for i in strr if strr.index(i)%2==0 ]
 print(*li,sep = ' ')
 
-print(strr[2].upper())
+b=[strr[x].upper() for x in range(1,len(strr)) if x%3==0]                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                           
 
 li2= [i[::-1] for i in strr]
 
 print(*li2,sep = ' ')
 
- 
 
+
+print(b) 
