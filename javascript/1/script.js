e38e632f5912158b56d78c51a31d51c50e813e5e
ujwalkpl <ujwlkpl@gmail.com>
2019-09-26 16:16:04 +0530

    added javascript

--- javascript/1/script.js
+++ javascript/1/script.js
@@ -2,7 +2,10 @@ function check() {
     var number = document.getElementById("number").value
     if (isNaN(number)) {
       alert("Enter a number");
-    } else if (number % 3 == 0 ){
+    } else if (number % 3 == 0 &&  number % 7 == 0  ){
+      document.getElementById("result").innerHTML = "Divisible by 3 and 7"
+    }
+    else if (number % 3 == 0  ){
       document.getElementById("result").innerHTML = "Divisible by 3"
     } else if (number % 7 == 0 ){
       document.getElementById("result").innerHTML = "Divisible by 7"
