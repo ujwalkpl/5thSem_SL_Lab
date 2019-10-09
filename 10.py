5a381301576b88e60abce3aa758bd2d6d152435a
Tilak <51227848+is124@users.noreply.github.com>
2019-10-09 17:33:26 +0530

    Update 10.py
    Keeping Key as string and later converting to Int.

--- 10.py
+++ 10.py
@@ -1,5 +1,5 @@
-studentdir = { 1:'Ujwal',2:'Uzma',3:'Vaibhav',4:'Vishwa'}
+studentdir = { '1':'Ujwal','2':'Uzma','3':'Vaibhav','4':'Vishwa'}
 
-a=[studentdir[i] for i in studentdir if i%2==0]
+a=[studentdir[i] for i in studentdir if int(i)%2==0]
 
 print(*a,sep=' ')
