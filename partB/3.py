c09b5d5e6ceba6eea020acd426ea767d6f9876eb
Ujwal Karippali Chandran <43445980+ujwalkpl@users.noreply.github.com>
2019-10-23 15:45:07 +0530

    Create 3.py

---
+++ partB/3.py
@@ -0,0 +1,9 @@
+class reverse_string:
+    def __init__(self,strr):
+        self.strr=strr
+    def reverse(self):
+        print(' '.join(self.strr.split()[::-1]))
+        
+obb1= reverse_string("i am here")
+obb2= reverse_string("hello everyone overthere")
+obb.reverse()
